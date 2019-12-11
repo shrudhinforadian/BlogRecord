5b3eea86b48d92aa7fd8c01ca29c9fcd8f9ba74e
commonman007 <shrudhinvraj1998@gmail.com>
2019-12-11 17:56:19 +0530

    routes updated

--- app.rb
+++ app.rb
@@ -22,11 +22,11 @@ class Main
       when 'comments/id'
         [200, { 'Content-Type' => 'text/json/html' }, [Comment.find_by(id: id).to_json.to_s]]
       when 'posts/comments'
-        [200, { 'Content-Type' => 'text/json/html' }, [Comment.find_by(id: id).to_json.to_s]]
+        [200, { 'Content-Type' => 'text/json/html' }, [Comment.find_all.to_json.to_s]]
       when 'comments?postid'
-        [200, { 'Content-Type' => 'text/json/html' }, [Comment.find_by(id: id).to_json.to_s]]
+        [200, { 'Content-Type' => 'text/json/html' }, [Comment.find_by(postid: id).to_json.to_s]]
       when 'posts?userid'
-        [200, { 'Content-Type' => 'text/json/html' }, [Comment.find_by(id: id).to_json.to_s]]
+        [200, { 'Content-Type' => 'text/json/html' }, [Post.find_by(userid: id).to_json.to_s]]
       when 'users/id'
         [200, { 'Content-Type' => 'text/json/html' }, [Users.find_by(id: id).to_json.to_s]]
       # [500, {"Content-Type" => "text/html"}, ["Goodbye Cruel World!"]]
@@ -34,12 +34,14 @@ class Main
         [404, { 'Content-Type' => 'text/html' }, ["<h1 align='center'>Error 404 not found </h1>"]]
       end
     when 'POST'
-
+      [201, { 'Content-Type' => 'text/json/html' }, [Post.create(:id => contact[:name] ,:userid =>  contact[:number],:title =>contact[:email],:body =>contact[:email] )]]
     when 'PUT'
-      
+
     when 'PATCH'
 
     when 'DELETE'
+      post=Post.find_by(id: id)
+      post.destroy
 
     end
   end
